8f666d2f62e2222a77372fb71c51bc5f26b5e58d
work@ya.com <work>
2024-12-11 17:22:01 +0300

    10

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
 import pandas as pd
-import numpy as np
 import os
-
-df = pd.read_excel("DZ_09.12.2024.xls", sheet_name="Дневное задание",header=0,index_col=None, usecols="A,B,F,K") 
-
-def naz(a:str):
-    print(a)
-
-ls=('Accutex Al-400SA','ARTA 152 NANO','Laser JPT100','Weld-CNC400','Аутсорсинг','Кооперация','Robodrill','Sunmill')
-
-df1=df[(df['Кол-во (факт)']==0) & (df['Программа']=='Нет') & (~df['Станок'].isin(ls) )]['Деталь']
-ls1=list(df1.values)
-# print(ls1)
-
-path_='..\\mk\\'
-
 
 def get_first_level_directories(root_dir):
     directories = []
@@ -28,19 +13,17 @@ def get_first_level_directories(root_dir):
         if os.path.isdir(full_path):
             directories.append(item)
             
-    return directories
-
-# Пример использования функции
-root_directory ='mk\\'
-first_level_dirs = get_first_level_directories(root_directory)
-# print(first_level_dirs)
-
-for i in ls1:
-    if i in first_level_dirs:
-        print(i)
-    else:
-        print('нет')
-        
+    return directories        
 
 if __name__ == '__main__':
-    pass
+    df = pd.read_excel("DZ_09.12.2024.xls", sheet_name="Дневное задание",header=0,index_col=None, usecols="A,B,F,K") 
+    ls=('Accutex Al-400SA','ARTA 152 NANO','Laser JPT100','Weld-CNC400','Аутсорсинг','Кооперация','Robodrill','Sunmill')
+    df1=df[(df['Кол-во (факт)']==0) & (df['Программа']=='Нет') & (~df['Станок'].isin(ls) )]['Деталь']
+    root_directory ='mk\\'
+    first_level_dirs = get_first_level_directories(root_directory)
+    for i in df1.values:
+        if i in first_level_dirs:
+            print(i)
+        else:
+            print('нет')
+
